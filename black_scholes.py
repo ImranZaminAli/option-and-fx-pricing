77da1981fd4b64246bb9e6b158ed0039bc5366d6
ImranZaminAli <izaminali@gmail.com>
2025-09-16 01:06:40 +0100

    started adding greeks

--- black_scholes.py
+++ black_scholes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import scipy.stats as stats
 
 def calculate_d1_d2(
@@ -6,12 +7,13 @@ def calculate_d1_d2(
     strike_price,
     risk_free_rate,
     volatility,
-    time_to_maturity):
+    time_to_maturity,
+    dividend_yield):
     # Log term: Represents the natural logarithm of the ratio of the current stock price to the strike price
     log_term = np.log(current_price / strike_price)
     
     # Drift term: Accounts for the expected return of the stock, adjusted for risk-free rate and volatility
-    drift_term = (risk_free_rate + 0.5 * volatility ** 2)
+    drift_term = (risk_free_rate - dividend_yield + 0.5 * volatility ** 2)
 
     # Scale the volatility by the square root of the time term
     time_scaled_volatility = volatility * time_to_maturity
@@ -26,16 +28,46 @@ def black_scholes_call(
     strike_price,
     risk_free_rate,
     volatility,
-    time_to_maturity):
-    d1, d2 = calculate_d1_d2(current_price, strike_price, risk_free_rate, volatility, time_to_maturity)
+    time_to_maturity,
+    dividend_yield):
+    d1, d2 = calculate_d1_d2(current_price, strike_price, risk_free_rate, volatility, time_to_maturity, dividend_yield)
     
     n_d1 = stats.norm.cdf(d1)  # Cumulative distribution function for d1
     n_d2 = stats.norm.cdf(d2)  # Cumulative distribution function for d2
 
     # Calculate the Black-Scholes price
-    call_price = (current_price * n_d1) - (strike_price * np.exp(-risk_free_rate * time_to_maturity) * n_d2)
+    call_price = (current_price * np.exp(-dividend_yield * time_to_maturity) * n_d1) - (strike_price * np.exp(-risk_free_rate * time_to_maturity) * n_d2)
     
     return call_price
+
+def greeks(current_price, strike_price, risk_free_rate, volatility, time_to_maturity, dividend_yield):
+    d_1, d2 = calculate_d1_d2(current_price, strike_price, risk_free_rate, volatility, time_to_maturity)
+    norm_prime_d1 = 1 / (np.sqrt(2 * np.pi)) * np.exp(-0.5 * d_1 ** 2)
+    q = np.exp(-dividend_yield * time_to_maturity)
+    gamma = calculate_gamma(current_price, volatility, time_to_maturity, d_1, norm_prime_d1, q)
+    theta = calculate_theta(current_price, volatility, time_to_maturity, norm_prime_d1, d2, risk_free_rate, strike_price, q)
+    vega = calculate_vega(current_price, time_to_maturity, norm_prime_d1, q)
+
+    data = {
+        'Delta': [d_1 * q],
+        'Gamma': [gamma],
+        'Theta': [theta],
+        'Vega': [vega]
+    }
+    df = pd.DataFrame(data)
+
+    return df
+
+def calculate_gamma(current_price, volatility, time_to_maturity, d1, norm_prime_d1, q):
+    return q * norm_prime_d1 / (current_price * volatility * np.sqrt(time_to_maturity))
+    
+
+def calculate_theta(current_price, volatility, time_to_maturity, norm_prime_d1, d2, risk_free_rate, strike_price, q):
+    return -  (current_price * q * norm_prime_d1 * volatility) / (2 * np.sqrt(time_to_maturity))- risk_free_rate * strike_price * np.exp(-risk_free_rate * time_to_maturity) * stats.norm.cdf(d2)
+
+def calculate_vega(current_price, time_to_maturity, norm_prime_d1, q):
+    return current_price * q * time_to_maturity * norm_prime_d1
+
 
 if __name__ == "__main__":
     # Example usage
